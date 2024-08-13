e195399f34272989c053824afd842cd593e349cb
tinepple <nastya.koptina@ya.ru>
2024-08-13 16:39:02 +0300

    Создание каркаса  handler

---
+++ internal/storage/struct.go
@@ -0,0 +1,31 @@
+package storage
+
+import (
+	"database/sql"
+	"time"
+)
+
+type House struct {
+	ID        int64          `db:"id"`
+	Address   string         `db:"addres"`
+	Year      int64          `db:"year"`
+	Developer sql.NullString `db:"developer"`
+	CreatedAt time.Time      `db:"created_at"`
+	UpdatedAt time.Time      `db:"updated_at"`
+}
+
+type Flat struct {
+	ID         int64         `db:"id"`
+	HouseID    int64         `db:"house_id"`
+	Status     string        `db:"status"`
+	Number     sql.NullInt64 `db:"number"`
+	Price      int64         `db:"price"`
+	RoomsCount int64         `db:"rooms_count"`
+}
+
+type User struct {
+	ID       int64  `db:"id"`
+	Email    string `db:"email"`
+	Password string `db:"password"`
+	Role     string `db:"role"`
+}
